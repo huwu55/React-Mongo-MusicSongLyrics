bf094bd5e146d337b8aea1e477ee56ab9ba22b5c
Huiling Wu <huwu@ucdavis.edu>
2019-03-14 00:01:11 -0700

    edited main page

--- client/src/pages/Intro.js
+++ client/src/pages/Intro.js
@@ -11,7 +11,7 @@ export default () => {
             <div className="intro">
                 <h1 className="title">Music, Song, &amp; Lyrics</h1>
                 <p>Pair artist and song to retrieve music lyrics and a video for you to sing along</p>
-                <p>Log in/Sign up to add your favorite songs and create playlists</p>
+                <p>Log in/Sign up to add your favorite songs</p>
             </div>
         </div>
     );
