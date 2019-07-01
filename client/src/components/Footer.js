242e7eb62e4e1f4b8c6c8325c398b1d1a78dd810
Huiling Wu <huwu@ucdavis.edu>
2019-07-01 16:02:58 -0700

    added github link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export default () => {
     return (
-        <footer>React Mongo MusicSongLyrics @Huiling Wu</footer>
+        <footer>
+            React Mongo MusicSongLyrics &copy;2019 Huiling Wu &nbsp;
+            <a title="Github" href="https://github.com/huwu55/React-Mongo-MusicSongLyrics" target="_blank"><i class="fab fa-github"></i></a>      
+        </footer>
     );
 }
